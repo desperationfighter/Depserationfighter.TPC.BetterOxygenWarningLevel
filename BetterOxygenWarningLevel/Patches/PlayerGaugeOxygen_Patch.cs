0172ec5a195c201921e96e9d0774aa83bcf053b2
Desperationfighter <patrick_as_desp@yahoo.de>
2023-11-04 14:41:14 +0100

    Add project files.

---
+++ BetterOxygenWarningLevel/Patches/PlayerGaugeOxygen_Patch.cs
@@ -0,0 +1,201 @@
+﻿using HarmonyLib;
+using SpaceCraft;
+using System;
+
+namespace BetterOxygenWarningLevel.Patches
+{
+    [HarmonyPatch(typeof(PlayerGaugeOxygen))]
+    [HarmonyPatch(nameof(PlayerGaugeOxygen.GaugeVerifications))]
+    public static class PlayerGaugeOxygen_GaugeVerifications_Patch
+    {
+        public static bool firstrundone;
+        public static bool halftimealreadytiggered;
+        public static bool SixtySecondWarningtriggered;
+
+        [HarmonyPrefix]
+        static bool Prefix(PlayerGaugeOxygen __instance) 
+        {
+            if(!Plugin.ModisActive.Value) return true;
+            //---------------------------------------------------------------
+            if (!firstrundone)
+            { 
+                firstrundone = true;
+                return true;
+            }
+
+            //Plugin.MyDebugLogger("We are Patching Oxygen Warnings");
+
+            //Todo - Make Check that Values are not lower then the next level
+            //Check 1 Low Level can not be lower than Critical
+            if (Plugin.LowWarning_Limit.Value <= Plugin.CriticalWarning_Limit.Value)
+            {
+                Plugin.LowWarning_Limit.Value = Plugin.CriticalWarning_Limit.Value + 5;
+            }
+
+            try
+            {
+                GaugeVerifications_Patched(__instance);
+            }
+            catch(Exception ex)
+            {
+                Plugin.Logger.LogError($"Error detected Fallback on Nativ Game Code. Error -> {ex.Message}");
+                return true;
+            }
+           
+            //---------------------------------------------------------------
+            return false;
+        }
+
+        static void GaugeVerifications_Patched(PlayerGaugeOxygen __instance)
+        {
+            if (!__instance.isInited)
+            {
+                return;
+            }
+
+            if (__instance.gaugeValue <= 20f)
+            {
+                __instance.playerAudio.SetIsPanting(true);
+            }
+            else
+            {
+                __instance.playerAudio.SetIsPanting(false);
+            }
+
+            try //to prevent issues and Break the Game. Let the Player at least play. Even its not that good in terms of Unity Error Handling...
+            {
+                if (Plugin.HalftimeWarning_Enable.Value)
+                {
+                    //Plugin.MyDebugLogger("Halftime - Enabled : True");
+                    if (__instance.maxValue > Plugin.HalftimeWarning_onlywhenMaxTankCapacyisover_Limit.Value && __instance.gaugeValue <= (__instance.maxValue / 2) )
+                    {
+                        //Plugin.MyDebugLogger($"Halftime - Innerhalf if true");
+                        if (!halftimealreadytiggered)
+                        {
+                            //Plugin.MyDebugLogger($"Halftime - Innerhalf triggered");
+                            Managers.GetManager<BaseHudHandler>().DisplayCursorText("UI_WARN_OXYGEN_HALF", 3f, "Oxygen half Capacity");
+                            if (Plugin.HalftimeWarning_withSignal.Value)
+                            {
+                                //Plugin.MyDebugLogger($"Halftime - Innerhalf Sound");
+                                __instance.globalAudioHandler.PlayAlertLow();
+                            }
+                            else
+                            {
+                                //Plugin.MyDebugLogger($"Halftime - Innerhalf without Sound");
+                            }
+                            halftimealreadytiggered = true;
+                        }
+                        else
+                        {
+                            //Plugin.MyDebugLogger($"Halftime - Innerhalf NOT triggered");
+                        }
+                    }
+                    else
+                    {
+                        //Plugin.MyDebugLogger($"Halftime - Innerhalf if false -> reset");
+                        halftimealreadytiggered = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Plugin.Logger.LogError("Error on running Halftime Oxygen Trigger");
+                Plugin.Logger.LogError(e.Message);
+            }
+            
+            try //to prevent issues and Break the Game. Let the Player at least play. Even its not that good in terms of Unity Error Handling...
+            {
+                if (Plugin.SixtySecWarning_Enable.Value)
+                {
+                    //Plugin.MyDebugLogger("Sixty - Enabled : True");
+                    if (__instance.maxValue > Plugin.SixtySecWarningonlywhenMaxTankCapacyisover_Limit.Value && __instance.gaugeValue <= Plugin.SixtySecWarning_Limit.Value)
+                    {
+                        //Plugin.MyDebugLogger("Sixty - Sixyty Limit reached : True");
+                        if (!SixtySecondWarningtriggered)
+                        {
+                            //Plugin.MyDebugLogger("Sixty - Sixyty Warning triggered : True");
+                            Managers.GetManager<BaseHudHandler>().DisplayCursorText("UI_WARN_OXYGEN_SIXTYSEC", 3f, "Oxygen Sixty Seconds");
+                            if (Plugin.SixtySecWarning_withSignal.Value)
+                            {
+                                //Plugin.MyDebugLogger("Sixty - With Sounds : True");
+                                __instance.globalAudioHandler.PlayAlertLow();
+                            }
+                            else
+                            {
+                                //Plugin.MyDebugLogger("Sixty - With Sounds : False");
+                            }
+                            SixtySecondWarningtriggered = true;
+                        }
+                        else
+                        {
+                            //Plugin.MyDebugLogger("Sixty - Sixyty Warning triggered : false");
+                        }
+                    }
+                    else
+                    {
+                        //Plugin.MyDebugLogger("Sixty - Sixyty Limit reached : false -> reset");
+                        SixtySecondWarningtriggered = false;
+                    }
+                }
+                else
+                {
+                    //Plugin.MyDebugLogger("Sixty - Enabled : false");
+                }
+            }
+            catch (Exception e)
+            {
+                Plugin.Logger.LogError("Error on running Sixty Seconds Oxygen Trigger");
+                Plugin.Logger.LogError(e.Message); 
+            }
+
+
+            if (__instance.gaugeValue > Plugin.LowWarning_Limit.Value)
+            {
+                __instance.hasAlertedLow = false;
+                __instance.hasAlertedCritical = false;
+                return;
+            }
+
+            if(Plugin.LowWarning_Enable.Value) 
+            {
+                if (__instance.gaugeValue <= Plugin.LowWarning_Limit.Value && !__instance.hasAlertedLow)
+                {
+                    Managers.GetManager<BaseHudHandler>().DisplayCursorText("UI_WARN_OXYGEN_LOW", 3f, "");
+                    if(Plugin.LowWarning_withSignal.Value)
+                    {
+                        if(Plugin.LowWarning_SignalSoundChangetoCritical.Value)
+                        {
+                            __instance.globalAudioHandler.PlayAlertCritical();
+                        }
+                        else 
+                        {
+                            __instance.globalAudioHandler.PlayAlertLow();
+                        }
+                    }
+                    __instance.hasAlertedLow = true;
+                    return;
+                }
+            }
+
+            if(Plugin.CritiaclWarning_Enable.Value)
+            {
+                if (__instance.gaugeValue <= Plugin.CriticalWarning_Limit.Value && !__instance.hasAlertedCritical)
+                {
+                    Managers.GetManager<BaseHudHandler>().DisplayCursorText("UI_WARN_OXYGEN_CRITICAL", 3f, "");
+                    if(Plugin.CritiaclWarning_withSignal.Value)
+                    {
+                        __instance.globalAudioHandler.PlayAlertCritical();
+                    }
+                    __instance.hasAlertedCritical = true;
+                    return;
+                }
+            }
+
+            if (__instance.gaugeValue <= 0f)
+            {
+                __instance.playerAudio.PlayDie();
+            }
+
+        }
+    }
+}
---
+++ BetterOxygenWarningLevel/Plugin.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using System.Linq;
+using BetterOxygenWarningLevel.Patches;
+
+namespace BetterOxygenWarningLevel
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+        public const string GUID = "Desperationfighter.TPC.BetterOxygenWarningLevel";
+        public const string Name = "Better Oxygen Warning Level";
+        public const string Version = "1.0.0.0"; //Remmber to Update Assembly Version too !
+
+        public static float MaxValue;
+
+        public static ConfigEntry<bool> ModisActive;
+        public static ConfigEntry<bool> Debuglogging;
+
+        public static ConfigEntry<bool> CritiaclWarning_Enable;
+        public static ConfigEntry<bool> CritiaclWarning_withSignal;
+        public static ConfigEntry<Single> CriticalWarning_Limit;
+
+        public static ConfigEntry<bool> LowWarning_Enable;
+        public static ConfigEntry<bool> LowWarning_withSignal;
+        public static ConfigEntry<bool> LowWarning_SignalSoundChangetoCritical;
+        public static ConfigEntry<Single> LowWarning_Limit;
+
+        public static ConfigEntry<bool> SixtySecWarning_Enable;
+        public static ConfigEntry<bool> SixtySecWarning_withSignal;
+        public static ConfigEntry<Single> SixtySecWarning_Limit;
+        public static ConfigEntry<Single> SixtySecWarningonlywhenMaxTankCapacyisover_Limit;
+
+        public static ConfigEntry<bool> HalftimeWarning_Enable;       
+        public static ConfigEntry<bool> HalftimeWarning_withSignal;
+        public static ConfigEntry<Single> HalftimeWarning_onlywhenMaxTankCapacyisover_Limit;
+
+        private void Awake()
+        {
+            ModisActive = Config.Bind("1_General", "ModisActive", true, "Set if the Mod should running or not. If you don't want to remove Files or for Later Ingame Menu. Please reload your Savegame after Change as there are Setting that only apply once when World is loaded up.");
+            Debuglogging = Config.Bind("9_Advanced", "Debuglogging", false, "Enables Debug Logging. Should be only activated when you know what you do.");
+
+            CritiaclWarning_Enable = Config.Bind("2_CriticalWarning", "CritiaclWarning_Enable", true, "");
+            CriticalWarning_Limit = Config.Bind("2_CriticalWarning", "CriticalWarning_Limit", 18f, "Game Default = 16f");
+            CritiaclWarning_withSignal = Config.Bind("2_CriticalWarning", "CritiaclWarning_withSignal", true, "");
+
+            LowWarning_Enable = Config.Bind("3_LowWarning", "CritiaclWarning_Enable", true, "");
+            LowWarning_Limit = Config.Bind("3_LowWarning", "LowWarning_Limit", 35f, "Game Default = 30f");
+            LowWarning_withSignal = Config.Bind("3_LowWarning", "LowWarning_withSignal", true, "");
+            LowWarning_SignalSoundChangetoCritical = Config.Bind("3_LowWarning", "LowWarning_SignalSoundChangetoCritical", true, "");
+
+            SixtySecWarning_Enable = Config.Bind("4_SixtySeconds", "SixtySecWarning_Enable", true, "");
+            SixtySecWarning_Limit = Config.Bind("4_SixtySeconds", "SixtySecWarning_Limit", 60f, "");
+            SixtySecWarning_withSignal = Config.Bind("4_SixtySeconds", "SixtySecWarning_withSignal", true, "");
+            SixtySecWarningonlywhenMaxTankCapacyisover_Limit = Config.Bind("4_SixtySeconds", "SixtySecWarningonlywhenMaxTankCapacyisover_Limit", 200f, "");
+
+            HalftimeWarning_Enable = Config.Bind("5_HalftimeWarning", "HalftimeWarning_Enable", true, "");
+            HalftimeWarning_withSignal = Config.Bind("5_HalftimeWarning", "HalftimeWarning_withSignal", false, "");
+            HalftimeWarning_onlywhenMaxTankCapacyisover_Limit = Config.Bind("5_HalftimeWarning", "HalftimeWarning_onlywhenMaxTankCapacyisover_Limit", 150f, "");
+
+
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            List<float> list = new List<float>();
+            list.Add(Plugin.CriticalWarning_Limit.Value);
+            list.Add(Plugin.LowWarning_Limit.Value);
+            list.Add(Plugin.SixtySecWarning_Limit.Value);
+            MaxValue = list.Max();
+
+            PlayerGaugeOxygen_GaugeVerifications_Patch.halftimealreadytiggered = false;
+            PlayerGaugeOxygen_GaugeVerifications_Patch.SixtySecondWarningtriggered = false;
+            PlayerGaugeOxygen_GaugeVerifications_Patch.firstrundone = false;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+            Logger.LogInfo($"Plugin {GUID} is loaded!");
+        }
+
+        public static void MyDebugLogger(string message)
+        {
+            if (Debuglogging.Value)
+            {
+                Logger.LogDebug($"[{Name}][Debug] : {message} [/Debug]");
+            }
+        }
+    }
+}
